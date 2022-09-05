dadd82cf50c716e457aad319c00bef7fb44d2dd7
EvedoesCS <leonardeve03@gmail.com>
2022-09-05 10:10:25 -0600

    Allow the generation of a Etch-a-Sketch grid An Etch-a-Sketch grid of the desired demensions can now be generated on page load.

---
+++ eas.js
@@ -0,0 +1,20 @@
+function gridLoad(resolution) 
+{
+    const gridContainer = document.getElementById('grid-container');
+
+    for (let i = 1; i <= resolution; i++) //Create a row
+    {
+        const row = document.createElement('div');
+
+        for(let j = 1; j <=resolution; j++) //Create squares per row
+        {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel-empty');
+            row.appendChild(pixel)
+        }
+        gridContainer.appendChild(row);
+    }
+}
+
+gridLoad(16);
+
